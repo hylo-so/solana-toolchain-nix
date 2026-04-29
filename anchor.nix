7a38466e3b35a03bd08638fcdbf869b38f387107
0xPlish <plish@hylo.so>
2026-04-29 08:37:54 -0700

    Bump anchor v0.32.1 -> v1.0.1

--- anchor.nix
+++ anchor.nix
@@ -1,18 +1,18 @@
 { anchor, fetchFromGitHub, rustPlatform, pkg-config, openssl, lib, stdenv
 , udev, }:
 anchor.overrideAttrs (old: rec {
-  version = "0.32.1";
+  version = "1.0.1";
   src = fetchFromGitHub {
     owner = "coral-xyz";
     repo = "anchor";
     tag = "v${version}";
-    hash = "sha256-oyCe8STDciRtdhOWgJrT+k50HhUWL2LSG8m4Ewnu2dc=";
+    hash = "sha256-lpLNocNrSWkf/b34PCmUKqFumdo3LcOyGMtN8O2ciEU=";
     fetchSubmodules = true;
   };
   cargoDeps = rustPlatform.fetchCargoVendor {
     inherit src;
     name = "anchor-${version}-vendor";
-    hash = "sha256-XrVvhJ1lFLBA+DwWgTV34jufrcjszpbCgXpF+TUoEvo=";
+    hash = "sha256-Nx5g+X9cPL71Gf9J/Zp5u6H8rrbDQW6KqTc/Ti+mzow=";
   };
   OPENSSL_NO_VENDOR = "1";
   nativeBuildInputs = old.nativeBuildInputs ++ [ pkg-config ];
